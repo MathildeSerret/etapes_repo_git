aee17e95149ba7e0f11d70e344aa20b6c30679c1
MathildeSerret <55735934+MathildeSerret@users.noreply.github.com>
2019-10-31 17:06:32 +0100

    Add files via upload

---
+++ repo git.rb
@@ -0,0 +1,13 @@
+faire un repo sur github via le terminal et clé https
+à la racine du dossier en ayant copé la clé https sur github: 
+
+git init
+git remote -v
+git remote add origin https://github.com/MathildeSerret/dancing_rails.git
+git add
+git commit -m "First Commit"
+git push
+
+puis copier ce qui s'inscrit, par exemple :  git push --set-upstream origin master
+
+
