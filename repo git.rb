15dc38ee3a4af85f0d9da7271203b760274605e4
MathildeSerret <55735934+MathildeSerret@users.noreply.github.com>
2019-11-26 11:54:15 +0100

    Update repo git.rb

--- repo git.rb
+++ repo git.rb
@@ -13,3 +13,5 @@ puis copier ce qui s'inscrit, par exemple :  git push --set-upstream origin mast
 voir les branches : git branch -a
 nouvelle branche : git branch test_mathilde
 aller sur une branche : git checkout test_mathilde
+mettre la nouvelle branche en remote : git push
+récupérer toutes les MAJ du github : git fetch --all
